d2d588e1b228e560a1dcc24ff8bca164b37911b0
Kay <kay.liang@outlook.com>
2020-07-13 18:00:38 -0400

    time travel hover

--- scripts/draggable.js
+++ scripts/draggable.js
@@ -1,4 +1,31 @@
+var positions = JSON.parse(localStorage.positions || "{}");
+$(function () {
+    var d = $("[id=draggable]").attr("id", function (i) {
+        return "draggable_" + i
+    })
+    $.each(positions, function (id, pos) {
+        $("#" + id).css(pos)
+    })
+
+    d.draggable({
+        containment: "#containment-wrapper",
+        scroll: false,
+        stop: function (event, ui) {
+            positions[this.id] = ui.position
+            localStorage.positions = JSON.stringify(positions)
+        }
+    });
+            
+});
+
 $(function() {
     $( ".draggable" ).draggable();
   });
+
+
+
+// inventory toggle
+// $('.charlie').dblclick(function() {
+//     $('#charlie-inventory').toggle();
+
 
